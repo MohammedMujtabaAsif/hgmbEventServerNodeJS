b0f81af8883b7d3d18d39186f1ff6ca01df2f56f
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2021-10-09 18:19:40 +0100

    Store eventid and websocket of User

--- user.js
+++ user.js
@@ -1,22 +1,41 @@
 class User {
     id;
     name;
-    active;
+    eventid;
 
-    constructor(id, name) {
+    constructor(id, name, eventid) {
         this.id = id;
         this.name = name;
+        this.setEventId(eventid)
+
+        var ws;
+
+        this.setWS = function (webs) {
+            ws = webs;
+        }
+
+        this.removeWS = function () {
+            ws = null;
+        }
+
+        this.getWS = function () {
+            return ws;
+        }
     }
 
-    setActive() {
-        this.active = true;
-        this.active ? true : false;
+
+    isActive() {
+        return this.getWS() == null ? false : true;
     }
 
-    setInactive() {
-        this.active = false;
-        !this.active ? true : false;
+    setEventId(eventid) {
+        this.eventid = eventid;
     }
+
+    removeEventId() {
+        this.eventid = null;
+    }
+
 }
 
 module.exports = User;
