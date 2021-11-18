b1acfddd574518dc743d4a09505063e602c2ad74
MohammedMujtabaAsif <asifmm@aston.ac.uk>
2021-11-18 00:13:15 +0000

    Removed unneeded code and Added comments

--- index.js
+++ index.js
@@ -25,9 +25,6 @@ wss.on('connection', function connection(ws, req) {
 
   // A method to send events with a specific type so they can be differentiated by the client
   ws.message = function message(type, data) {
-    // console.log('sending message:');
-    // console.log(`type: ${type}`);
-    // console.log(data);
     var json = JSON.stringify({ type: type, data: data });
     console.log(json);
     ws.send(json);
@@ -39,116 +36,127 @@ wss.on('connection', function connection(ws, req) {
 
   ws.message('connection', 'Welcome to the Event');
 
+  // Handle requests received from the clients
   ws.listen = function listen(json) {
     let parsed;
+
+    // Parse the request from the client
     try {
       parsed = JSON.parse(json);
     } catch (error) {
+      // If the message was not sent as json then notify the client
       ws.message('error', 'Message sent in invalid format');
       console.log('Message received in invalid format');
     }
-    // console.log(parsed)
+    // Split the data from the JSON message
     let type = parsed.type;
     let data = parsed.data;
-    console.log(type);
-    switch (type) {
-      case 'join-event':
-        console.log('user attempting to join event')
-        var token = data.token;
-        // console.log(token)
-        getApiEvent(token).then((data) => {
-          // console.log('data from method: ');
-          console.log(data);
-          var success = false;
-          if (data != null) {
 
+    // Trigger events based upon the type key in the json message
+    switch (type) {
+
+      // When a user joins the event
+      case 'join-event':
+
+        // Log to the console that a user attempted to join
+        console.log('user attempting to join event')
+
+        // Pull the api token from the data sent
+        var token = data.token;
+
+        // Request the event information from the api
+        getApiEvent(token).then((data) => {
+          // Set success to be false by default
+          var success = false;
+
+          // If the api responded with data
+          if (data != null) {
+            // Split the json key data
             var eventid = data.id;
             var userid = data.user.id;
 
+
+            // Create a new event object
             var event = createEvent(data);
+            // Add the event object to the list of events being tracked
             addEventToEventsMap(eventid, event);
 
+            // Attempt the set the user as an active participant in the event
+            // If successful, overwrite the success variable above
             success = joinEvent(eventid, userid, socketid, ws);
           } else {
+            // If no data is received, send a fail message to the client
             ws.message('joined-event-fail', 'No Active Event Found')
           }
-          
+
+          // If the user successfully joined the event
           if (success) {
-            ws.message('joined-event-success', 'Welcome');
+            // Notify the client device that they joined the event
+            ws.message('joined-event-success', 'Welcome to the event');
+            // Notify the other users of the event that a new user has joined
             messageActiveUsersOfEvent(eventid, true);
           } else {
+            // Notify the client device that they failed to join the event
             ws.message('joined-event-fail', 'Failed to join event');
+            // Notify the other clients that the user is no longer in the event, in case there was a desync
             messageActiveUsersOfEvent(eventid, false);
           }
         });
         break;
+
+      // When a user leaves the event through in app actions
       case 'leave-event':
         console.log('user leaving event')
+        // Remove from the active users map and unset their websocket id
+        // Handled by setUserAsLeft(socketid)
         setUserAsLeft(socketid);
         break;
-      // case 'get-appointment':
-      //   sendCurrentAppointment(data);
-      //   break;
 
-      // case 'get-next-appointment':
-      //   sendNextAppointment(data);
-      //   break;
-      // case 'get-event':
-
-      //   break;
-
+      // When a user pings the server to ensure they are connected
       case 'check-connected':
         console.log('user checking they are connected');
+        // Response with a positive connected response message
         ws.message('connected', true);
         break;
+
+      // Wehn the type isn't matched with an event above
       default:
-        console.log("couldn't process message");
+        // Alert the client device that the request could not be handled
+        ws.message('error', 'Request type not supported');
+
+        // Print an alert to the console showing the type variable
+        console.log("couldn't process message:");
+        console.log(type);
         break;
     }
   }
 
+  // Listen for requests from clients
   ws.on('message', function (message) {
-    // console.log(message);
+    // Send the received data to the handler method
     ws.listen(message);
   });
 
+  // When a user disconnects from the websocket
   ws.on('close', function close(code, reason) {
     console.log('user disconnected with code: ' + code + ', reason: ' + reason);
     console.log(socketid);
+
+    // Find the eventid of the user that left
     let eventid = getUserFromActiveUsersMap(socketid).eventid;
+
+    // Remove the user from the list of active users
     setUserAsLeft(socketid);
+
+    // Notify all other users that they have left
     messageActiveUsersOfEvent(eventid, false);
   });
-
-
-  function sendCurrentAppointment(data) {
-    var eventid = parseInt(data.eventid);
-    getApiUser(data.token).then((res) => {
-      let userid = res.id;
-      console.log(`user ${userid} requested active appointment for event ${eventid}`);
-      console.log(`eventid: ${eventid}, socketid: ${socketid}`)
-      var appointment = getActiveAppointmentForUser(eventid, socketid);
-      ws.message('send-appointment', appointment);
-    });
-  }
-
-
-  function sendNextAppointment(data) {
-    var eventid = parseInt(data.eventid);
-    getApiUser(data.token).then((res) => {
-      let userid = res.id;
-      console.log(`user ${userid} requested next appointment for event ${eventid}`);
-      console.log(`eventid: ${eventid}, socketid: ${socketid}`)
-      var appointment = getNextAppointmentForUser(eventid, socketid);
-      ws.message('send-next-appointment', appointment);
-    });
-
-  }
 
   function messageActiveUsersOfEvent(eventid, joined) {
     let users = getUsersForEvent(eventid);
     let activeUsers = getActiveUsersIdsForEvent(eventid);
     let s = joined ? 'user-joined' : 'user-left';
+
     activeUsers.forEach((userid) => {
       let user = users.get(userid);
       if (user.getWS() != null) {
@@ -174,7 +182,7 @@ function removeUserFromActiveUsersMap(socketid) {
   usersockets.delete(socketid);
 }
 
-
+// Create and return a new event from the json data provided
 function createEvent(json) {
   let e = getEventFromEventsMap(json['id']);
   if (e == null) {
@@ -188,16 +196,15 @@ function addEventToEventsMap(eventid, event) {
   let size = events.size;
   events.set(eventid, event);
 
-  // return a true value if the event map increased in size by 1
+  // Return a true value if the event map increased in size by 1
   size + 1 == events.size ? true : false;
 }
 
-// retrieve the event with the specified id. if no event exists then return false. 
+// Retrieve the event with the specified id. if no event exists then return false. 
 function getEventFromEventsMap(eventid) {
   console.log(`Event id is ${eventid}`);
   console.log(events);
   let event = events.get(eventid)
-  // console.log(this.event);
   if (event != null) {
     console.log(`Event ${eventid} found`);
     return event;
@@ -205,7 +212,7 @@ function getEventFromEventsMap(eventid) {
   return null;
 }
 
-// retrieve the users for an event with the specified id from the Events map.
+// Retrieve the users for an event with the specified id from the Events map.
 function getUsersForEvent(eventid) {
   let users = getEventFromEventsMap(eventid).users;
   if (users != null) {
@@ -215,7 +222,7 @@ function getUsersForEvent(eventid) {
   return null;
 }
 
-// get a user from an event based upon the event and user id
+// Get a user from an event based upon the event and user id
 function getUserFromEvent(eventid, userid) {
   let users = getUsersForEvent(eventid);
   var user;
@@ -229,7 +236,8 @@ function getUserFromEvent(eventid, userid) {
   return null;
 }
 
-// 
+// Remove the ws id from the user model to set them as inactive
+// Remove user from the active users map
 function setUserAsLeft(socketid) {
   let user = getUserFromActiveUsersMap(socketid);
   user.removeWS();
@@ -237,7 +245,8 @@ function setUserAsLeft(socketid) {
   console.log(user);
 }
 
-// 
+// Add the user to the event they are joining
+// Set the user's websocket id
 function joinEvent(eventid, userid, socketid, websocket) {
   console.log(`${eventid}  ${userid}  ${socketid}`);
   let user = getUserFromEvent(eventid, userid);
@@ -252,7 +261,7 @@ function joinEvent(eventid, userid, socketid, websocket) {
   return false
 }
 
-// retrieve the appointments for an event with the specified id from the Events map.
+// Retrieve the appointments for an event with the specified id from the Events map.
 function getAppointmentsForEvent(eventid) {
   let event = getEventFromEventsMap(eventid);
   let appointments;
@@ -264,62 +273,7 @@ function getAppointmentsForEvent(eventid) {
   return appointments;
 }
 
-// 
-function getAppointmentsForUser(eventid, socketid) {
-  // console.log(getEvent(eventid))
-  let apps = getAppointmentsForEvent(eventid);
-  console.log(getUserFromActiveUsersMap(socketid));
-  let userid = getUserFromActiveUsersMap(socketid).id;
-  let userApps = [];
-  if (apps != null) {
-    console.log('searching appointments');
-    apps.forEach((app) => {
-      console.log(`checking ${app.id}'s sender (${app.sender.id}) and recipient (${app.recipient.id})`)
-      if (app.sender.id == userid || app.recipient.id == userid) {
-        userApps.push(app);
-      }
-    })
-  }
-  return userApps;
-}
-
-// Find the appointment for the user that started less than 4 minutes and 30 seconds ago
-function getActiveAppointmentForUser(eventid, socketid) {
-  let now = moment();
-  let apps = getAppointmentsForUser(eventid, socketid);
-  for (var i = 0; i < apps.length; i++) {
-    let app = apps[i];
-    let timeEnd = moment(app.time, 'DD/MM/YYYY HH:mm').add(4, 'minutes').add(30, 'seconds');
-
-    if (now.isBefore(timeEnd) && now.isAfter(app.time)) {
-      console.log('Found active appointment');
-      console.log(app);
-      return app;
-    }
-  }
-  console.log('No active appointment found')
-  return null;
-}
-
-// Find the next appointment
-function getNextAppointmentForUser(eventid, socketid) {
-  let now = moment();
-  let apps = getAppointmentsForUser(eventid, socketid);
-  for (var i = 0; i < apps.length; i++) {
-    let app = apps[i];
-    let timeStart = moment(app.time, 'DD/MM/YYYY HH:mm').subtract(5, 'seconds');
-    console.log(`App Time: ${timeStart}`);
-    console.log(`Now Time: ${now}`)
-    if (timeStart.isAfter(now)) {
-      console.log('Found next appointment');
-      console.log(app);
-      return app;
-    }
-  }
-  console.log('No next appointment found')
-  return null;
-}
-
+// Collect all the active user's ids for a specific event
 function getActiveUsersIdsForEvent(eventid) {
   let event = getEventFromEventsMap(eventid);
   let activeUsers = [];
@@ -332,8 +286,7 @@ function getActiveUsersIdsForEvent(eventid) {
   return activeUsers;
 }
 
-
-
+// Make an API request for the active user's event data
 async function getApiEvent(token) {
   console.log('getting active event from api request');
   var api = new ApiRequests(token);
@@ -347,24 +300,5 @@ async function getApiEvent(token) {
     }
   }));
   console.log('event data received');
-  // console.log(eventData);
   return eventData;
-}
-
-async function getApiUser(token) {
-  console.log(`getting user's ID from api request`);
-  var api = new ApiRequests(token);
-  var user = await api.getUser().then(data => {
-    console.log('response received');
-    if (!data['success']) {
-      console.log(data['message']);
-      return null;
-    } else {
-      return data['data'];
-    }
-  });
-  console.log('user data received');
-  // console.log(eventData);
-  return user;
-
 }
